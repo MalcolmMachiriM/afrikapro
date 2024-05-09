e6fa290aae3c85a110f9de4f02992c39e5655b44
Malcolm <machirimalcolm@gmail.com>
2024-05-09 10:02:24 +0200

    --change background image --added most home elements

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ import Link from "next/link";
 export default function Home() {
   return (
     <main className='min-h-fit min-w-fit py-8 sm:py-16relative'>
-      <div className='h-[700px] sm:h-[890px] w-full bg-cover bg-center absolute inset-0 -z-10' style={{backgroundImage: "url(/backg.jpeg)"}}>
+      <div className='h-[700px] sm:h-[890px] w-full bg-cover bg-center absolute inset-0 -z-10' style={{backgroundImage: "url(/deet.png)"}}>
         
       </div>
       <div className="SectionContainer">
@@ -55,7 +55,97 @@ export default function Home() {
               <img src="/versale_laga.png" alt="versal logo" loading="lazy"  width="1272" height="288" decoding="async" className="object-contain h-20 w-full mb-4"style={{color:"transparent"}}/>
             </Link>
             <aside>
-            <div className="bg-brown text-center py-4"><span className="text-2xl font-bold">2023 Ranking</span></div>
+              <div className="bg-brown text-center py-4"><span className="text-2xl font-bold">2023 Ranking</span>
+              </div>
+              <div className="flex items-center bg-gray p-2 text-textOffBlack">
+                <div className="avatar">
+                    <div className="w-20">
+                      <img alt="Team Profile" loading="lazy" width="128" height="128" decoding="async" data-nimg="1" className="border border-leaderboardActiveGray rounded-lg" style={{color:"transparent"}} src="/images/ayo.jpg">
+
+                      </img>
+                  </div>
+                </div>
+                <div className="pl-2 flex-1">
+                <span className="font-heebo font-medium text-sm">Champion Fancier</span>
+                  <div className="flex space-x-2 items-center">
+                    <img alt="country flag" loading="lazy" width="20" height="23" decoding="async" data-nimg="1" className="rounded-sm h-min" style={{color:"transparent"}} src="https://flagcdn.com/za.svg"></img>
+                    <span className="text-lg font-bold line-clamp-1">Pieter &amp; JL</span>
+                  </div>
+                </div>
+                <img src="/images/black_blazer.png" alt="competition icon" className="rounded-full w-20 h-20 object-contain"></img>
+              </div>
+              <div className="flex items-center bg-tableGray p-2 text-textOffBlack">
+                <div className="avatar">
+                    <div className="w-20">
+                      <img alt="Team Profile" loading="lazy" width="128" height="128" decoding="async" data-nimg="1" className="border border-leaderboardActiveGray rounded-lg" style={{color:"transparent"}} src="/images/ben.jpg">
+
+                      </img>
+                  </div>
+                </div>
+                <div className="pl-2 flex-1">
+                <span className="font-heebo font-medium text-sm">Champion Fancier</span>
+                  <div className="flex space-x-2 items-center">
+                    <img alt="country flag" loading="lazy" width="20" height="23" decoding="async" data-nimg="1" className="rounded-sm h-min" style={{color:"transparent"}} src="https://flagcdn.com/za.svg"></img>
+                    <span className="text-lg font-bold line-clamp-1">Pieter &amp; JL</span>
+                  </div>
+                </div>
+                <img src="/images/blue_blazer.png" alt="competition icon" className="rounded-full w-20 h-20 object-contain"></img>
+              </div>
+              <div className="flex items-center bg-tableGray p-2 text-textOffBlack">
+                <div className="avatar">
+                    <div className="w-20">
+                      <img alt="Team Profile" loading="lazy" width="128" height="128" decoding="async" data-nimg="1" className="border border-leaderboardActiveGray rounded-lg" style={{color:"transparent"}} src="/images/cri.jpg">
+
+                      </img>
+                  </div>
+                </div>
+                <div className="pl-2 flex-1">
+                <span className="font-heebo font-medium text-sm">Champion Fancier</span>
+                  <div className="flex space-x-2 items-center">
+                    <img alt="country flag" loading="lazy" width="20" height="23" decoding="async" data-nimg="1" className="rounded-sm h-min" style={{color:"transparent"}} src="https://flagcdn.com/za.svg"></img>
+                    <span className="text-lg font-bold line-clamp-1">Pieter &amp; JL</span>
+                  </div>
+                </div>
+                <img src="/images/country_trophy.png" alt="competition icon" className="rounded-full w-20 h-20 object-contain"></img>
+              </div>
+              <div className="flex items-center bg-tableGray p-2 text-textOffBlack">
+                <div className="avatar">
+                    <div className="w-20">
+                      <img alt="Team Profile" loading="lazy" width="128" height="128" decoding="async" data-nimg="1" className="border border-leaderboardActiveGray rounded-lg" style={{color:"transparent"}} src="/images/mic.jpg">
+
+                      </img>
+                  </div>
+                </div>
+                <div className="pl-2 flex-1">
+                <span className="font-heebo font-medium text-sm">Champion Fancier</span>
+                  <div className="flex space-x-2 items-center">
+                    <img alt="country flag" loading="lazy" width="20" height="23" decoding="async" data-nimg="1" className="rounded-sm h-min" style={{color:"transparent"}} src="https://flagcdn.com/za.svg"></img>
+                    <span className="text-lg font-bold line-clamp-1">Pieter &amp; JL</span>
+                  </div>
+                </div>
+                <img src="/images/race_trophy.png" alt="competition icon" className="rounded-full w-20 h-20 object-contain"></img>
+              </div>
+              <div className="flex items-center bg-tableGray p-2 text-textOffBlack">
+                <div className="avatar">
+                    <div className="w-20">
+                      <img alt="Team Profile" loading="lazy" width="128" height="128" decoding="async" data-nimg="1" className="border border-leaderboardActiveGray rounded-lg" style={{color:"transparent"}} src="/images/vin.jpg">
+
+                      </img>
+                  </div>
+                </div>
+                <div className="pl-2 flex-1">
+                <span className="font-heebo font-medium text-sm">Champion Fancier</span>
+                  <div className="flex space-x-2 items-center">
+                    <img alt="country flag" loading="lazy" width="20" height="23" decoding="async" data-nimg="1" className="rounded-sm h-min" style={{color:"transparent"}} src="https://flagcdn.com/za.svg"></img>
+                    <span className="text-lg font-bold line-clamp-1">Pieter &amp; JL</span>
+                  </div>
+                </div>
+                <img src="/images/weekly_trophy.png" alt="competition icon" className="rounded-full w-20 h-20 object-contain"></img>
+              </div>
+              <Link href="" className="py-4 bg-black flex justify-center my-2 mx-4 sm:mx-0 globalBrownHover cursor-pointer select-none">
+                <span className="mr-8"> View Full Table</span>
+                <img src="/svgs/white_arrow_right.svg" alt="arrow" className="w-5 h-auto"/>
+              </Link>
             </aside>
           </div>
         </section>
